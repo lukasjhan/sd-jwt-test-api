2fd778f526165400b0ae23f22ecc4419b251e4db
tree <hhyjs2r@gmail.com>
2024-03-06 15:19:03 +0900

    wip: test lists

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,36 @@
-import { serve } from '@hono/node-server';
-import { Hono } from 'hono';
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
 
 const app = new Hono();
 
-app.get('/', (c) => {
-  return c.json({ result: 'hello world!' });
+/**
+ * tests data has test names of each cases
+ */
+const tests = {
+  issue: ["testa", "testb"],
+  decode: [],
+  present: [],
+  verify: [],
+};
+
+const endpoints = [
+  "/test-lists",
+  "/keypairs",
+  "/tests/issue/{test-name}",
+  "/tests/present/{test-name}",
+  "/tests/verify/{test-name}",
+  "/tests/decode/{test-name}",
+  "/tests/present/{test-name}",
+  "/tests/issue/{test-name}",
+  "/tests/decode/{test-name}",
+];
+
+app.get("/test-lists", (c) => {
+  return c.json({ result: endpoints });
 });
 
-app.post('/', async (c) => {
-  return c.json({ result: 'hello world!' });
+app.post("/", async (c) => {
+  return c.json({ result: "hello world!" });
 });
 
 const port = 5600;
