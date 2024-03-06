d684c6fa1ffa000c7322e2d516e6909d8e0acc25
tree <hhyjs2r@gmail.com>
2024-03-06 15:32:42 +0900

    feat: remove unused file

--- src/schema.ts
+++
@@ -1,7 +0,0 @@
-import { sqliteTable, integer, text } from 'drizzle-orm/sqlite-core';
-
-export const users = sqliteTable('users', {
-  id: integer('id').primaryKey(),
-  name: text('name').notNull(),
-  age: integer('age'),
-});
