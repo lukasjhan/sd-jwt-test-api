9eb61f202b9f10560e12293974970d8b8be4e2e6
tree <hhyjs2r@gmail.com>
2024-03-07 14:41:41 +0900

    feat: add an error message

--- src/index.ts
+++ src/index.ts
@@ -42,14 +42,23 @@ app.get('/tests/issue/p1', (c) => {
   });
 });
 
+// answer : 'eyJhbGciOiJFUzI1NiJ9.eyJsYXN0bmFtZSI6IkRvZSIsInNzbiI6IjEyMy00NS02Nzg5IiwiZGF0YSI6eyJmaXJzdG5hbWUiOiJKb2huIiwibGFzdG5hbWUiOiJEb2UiLCJzc24iOiIxMjMtNDUtNjc4OSIsIl9zZCI6WyIzbzBmYVlnaGpnSUFEcnlacHh5WjlLMGdXVEt6SXNUamJBZWZ5Z2RtZFZJIiwiRFhueUVxa25oSHdkcmpKbzZ2UWpkVXA5bnI5WGdVVldHeWhJbkxpdktsQSIsInJ3X3BnVmZ0OEJsc3FTQ3Nnczk4OXN6aVNnY0loNzFwRGw1VDI2YzNUNDQiXX0sIl9zZCI6WyJGVUtCdW81WXFSbmY2N0VhZzV5RHAxeVVpb2xCSEZnQmEweGc1MWdKTGI4IiwiaTNubWw0YW9GNFZZZUt6NUdZdnVjeXJ5VDM4TlVxUG00QXZaTzY4eUVNTSIsImtWVU1nX1hGcjBwamdkbTl1cFpUbTgxVkVjaWF5LTJaOWd2Rmo5RkhBWXMiXSwiX3NkX2FsZyI6IlNIQS0yNTYifQ.927feVeSgpUkXqleM6fq1xRNUWsmqFDKjU4sWlT4UUPk3SLKKETCscP7eLbldXBugjQ11H4GKnCxSLPsbRsLDA~WyI5YjQzZTg3MTUzNmZkOTBmIiwiciIsIjEiXQ~WyIwZjVlZDQzNjU0YTBhZGFlIix7Il9zZCI6WyItd3A2cUdCSzVsVExLbTloY2JnenYxT2tUdnZyYnFoVDFBTDlEd3ZPNjVjIl19XQ~WyI4NzIwMTk3NDlhY2Q5NTdlIiwiYyJd~WyIwNGRlYjY0MmI2OWI1ZTAwIiwibGlzdCIsW3siLi4uIjoiTWlaTWNuSUs5bmE2aW9qcTBRZ00xREQxOENRaklGRl83cFFqamNIZ0RlcyJ9LCJiIix7Ii4uLiI6InpsNXd6QllKcDNUTjN4TzFNUlNWbURoTVZIajhQTHZ4cUwxUVBYeks4Vk0ifSx7Ii4uLiI6Imd1djBEZ3ZSSFh1bjN5dFdfWndWQWpHT05kNE40eDVKOHZpVFFlOVJ5RDgifV1d~WyJjMzEyOWJlZTU4NWRiZGU4IiwiaGkiLCJieWUiXQ~WyJmODk4ZWE0MTQwYjRmMjNiIiwiZmlyc3RuYW1lIiwiSm9obiJd~WyIyMzY3YWE1YmIyYjc1MGRlIiwiaWQiLCIxMjM0Il0~WyIzMTEyYTRjNWE1NzI3YWQyIiwiZGF0YTIiLHsiX3NkIjpbIlpKNWFUSi1QeUZKcEMtTmNMUWpnbzFnclFGN0hZYkF0QVY1YlNBbGh0dTQiXX1d~'
+
 app.post('/tests/issue/p1', async (c) => {
   const { answer } = await c.req.json<{ answer: string }>();
 
   if (!answer) {
-    throw new HTTPException(400, { message: 'bad request' });
+    throw new HTTPException(400, { message: 'bad request : You cant leave the body value blank.' });
+  }
+
+  if (typeof answer !== 'string') {
+    throw new HTTPException(400, {
+      message: 'bad request You didnt put an object or JSON type in there, did you? You need to insert a token!',
+    });
   }
 
   const submittedClaims = await sdjwt.getClaims(answer);
+  console.log(submittedClaims);
   const isCorrect = isEqual(submittedClaims, claims);
 
   return c.json({
@@ -76,8 +85,7 @@ app.post('/tests/present/p1', async (c) => {
     throw new HTTPException(400, { message: 'bad request' });
   }
 
-  const submittedPresentaion = await sdjwt.getClaims(answer);
-  const isCorrect = isEqual(submittedPresentaion, presentationClaim);
+  const isCorrect = isEqual(answer, presentationClaim);
   return c.json({ isCorrect });
 });
 
