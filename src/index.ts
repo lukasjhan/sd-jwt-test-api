dee068a650b8d0573ae0b8967a2d3349ed213161
seorim <94429667+yunseorim1116@users.noreply.github.com>
2024-03-12 17:18:32 +0900

    feat: add test case (#9)

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,27 @@
 import { serve } from '@hono/node-server';
 import { SDJwtInstance } from '@sd-jwt/core';
-import {
-  claims,
-  disclosureFrame,
-  presentationClaim,
-  presentationFrame,
-  credential,
-  presentableKeyList,
-} from './datas/claim';
-import { endpoints } from './datas/endpoint';
 import { Hono } from 'hono';
 import isEqual from 'lodash.isequal';
 import { ES256, digest, generateSalt } from '@sd-jwt/crypto-nodejs';
 import { HTTPException } from 'hono/http-exception';
 import { KeyPair } from './key';
+import { testcases as issueTestCases } from './cases/issues';
+import { testcases as decodeTestCases } from './cases/decodes';
+import { testcases as verifyTestCases } from './cases/verifies';
+import { testcases as presentTestCases } from './cases/presents';
 
 const app = new Hono();
 
-let sdjwt: undefined | SDJwtInstance<Record<string, unknown>>;
+/**
+ * make sdjwt Instance
+ */
 
+const decodeNames = Object.keys(decodeTestCases);
+const issuedNames = Object.keys(issueTestCases);
+const presentedNames = Object.keys(presentTestCases);
+const verifyedNames = Object.keys(verifyTestCases);
+
+let sdjwt: undefined | SDJwtInstance<Record<string, unknown>>;
 const getSDJwt = async () => {
   if (sdjwt) {
     return sdjwt;
@@ -36,144 +39,142 @@ const getSDJwt = async () => {
   return sdjwt;
 };
 
-/**
- * tests data has test names of each cases
- */
-
-const port = 5600;
-console.log(`Server is running on port ${port}`);
-
-const tests = {
-  issue: ['testa', 'testb'],
-  decode: [],
-  present: [],
-  verify: [],
-};
-
-app.get('/test-lists', (c) => {
-  return c.json({ result: endpoints });
-});
-
-/*
- * 문제1 : claims 와 disclosureFrame 줄테니까 토큰 발급해봐
- */
-
-app.get('/test/issue/p1', (c) => {
-  return c.json({
-    description: 'Issue a SD-JWT token with given claims and disclosureFrame',
-    claims,
-    disclosureFrame,
-  });
-});
-
-// answer : 'eyJhbGciOiJFUzI1NiJ9.eyJsYXN0bmFtZSI6IkRvZSIsInNzbiI6IjEyMy00NS02Nzg5IiwiZGF0YSI6eyJmaXJzdG5hbWUiOiJKb2huIiwibGFzdG5hbWUiOiJEb2UiLCJzc24iOiIxMjMtNDUtNjc4OSIsIl9zZCI6WyIzbzBmYVlnaGpnSUFEcnlacHh5WjlLMGdXVEt6SXNUamJBZWZ5Z2RtZFZJIiwiRFhueUVxa25oSHdkcmpKbzZ2UWpkVXA5bnI5WGdVVldHeWhJbkxpdktsQSIsInJ3X3BnVmZ0OEJsc3FTQ3Nnczk4OXN6aVNnY0loNzFwRGw1VDI2YzNUNDQiXX0sIl9zZCI6WyJGVUtCdW81WXFSbmY2N0VhZzV5RHAxeVVpb2xCSEZnQmEweGc1MWdKTGI4IiwiaTNubWw0YW9GNFZZZUt6NUdZdnVjeXJ5VDM4TlVxUG00QXZaTzY4eUVNTSIsImtWVU1nX1hGcjBwamdkbTl1cFpUbTgxVkVjaWF5LTJaOWd2Rmo5RkhBWXMiXSwiX3NkX2FsZyI6IlNIQS0yNTYifQ.927feVeSgpUkXqleM6fq1xRNUWsmqFDKjU4sWlT4UUPk3SLKKETCscP7eLbldXBugjQ11H4GKnCxSLPsbRsLDA~WyI5YjQzZTg3MTUzNmZkOTBmIiwiciIsIjEiXQ~WyIwZjVlZDQzNjU0YTBhZGFlIix7Il9zZCI6WyItd3A2cUdCSzVsVExLbTloY2JnenYxT2tUdnZyYnFoVDFBTDlEd3ZPNjVjIl19XQ~WyI4NzIwMTk3NDlhY2Q5NTdlIiwiYyJd~WyIwNGRlYjY0MmI2OWI1ZTAwIiwibGlzdCIsW3siLi4uIjoiTWlaTWNuSUs5bmE2aW9qcTBRZ00xREQxOENRaklGRl83cFFqamNIZ0RlcyJ9LCJiIix7Ii4uLiI6InpsNXd6QllKcDNUTjN4TzFNUlNWbURoTVZIajhQTHZ4cUwxUVBYeks4Vk0ifSx7Ii4uLiI6Imd1djBEZ3ZSSFh1bjN5dFdfWndWQWpHT05kNE40eDVKOHZpVFFlOVJ5RDgifV1d~WyJjMzEyOWJlZTU4NWRiZGU4IiwiaGkiLCJieWUiXQ~WyJmODk4ZWE0MTQwYjRmMjNiIiwiZmlyc3RuYW1lIiwiSm9obiJd~WyIyMzY3YWE1YmIyYjc1MGRlIiwiaWQiLCIxMjM0Il0~WyIzMTEyYTRjNWE1NzI3YWQyIiwiZGF0YTIiLHsiX3NkIjpbIlpKNWFUSi1QeUZKcEMtTmNMUWpnbzFnclFGN0hZYkF0QVY1YlNBbGh0dTQiXX1d~'
-
-app.post('/test/issue/p1', async (c) => {
-  const sdjwt = await getSDJwt();
-  const { answer } = await c.req.json<{ answer: string }>();
-
-  if (!answer) {
-    throw new HTTPException(400, { message: 'bad request : You cant leave the body value blank.' });
+app.get('/tests/decode/:name', (c) => {
+  const description = 'I will give you token, you have to send me a claim!';
+  const name = c.req.param('name') as keyof typeof decodeTestCases;
+  if (!decodeNames.includes(name)) {
+    throw new HTTPException(404, { message: 'test not found' });
   }
 
-  if (typeof answer !== 'string') {
-    throw new HTTPException(400, {
-      message: 'bad request You didnt put an object or JSON type in there, did you? You need to insert a token!',
-    });
+  const { credential } = decodeTestCases[name];
+
+  return c.json({
+    results: {
+      credential,
+      description,
+    },
+  });
+});
+
+app.post(`/tests/decode/:name`, async (c) => {
+  const name = c.req.param('name') as keyof typeof decodeTestCases;
+  if (!decodeNames.includes(name)) {
+    throw new HTTPException(404, { message: 'test not found' });
   }
 
-  const submittedClaims = await sdjwt.getClaims(answer);
-  // TODO: verify.
-  const isCorrect = isEqual(submittedClaims, claims);
+  const { claims } = decodeTestCases[name];
 
-  return c.json({
-    result: isCorrect,
-  });
-});
-
-/*
- * 문제2 : credential,
-    presentationFrame 을 줄테니
-    present한 토큰 결과값을 반환해.
- */
-app.get('/test/present/p1', async (c) => {
-  return c.json({
-    description: 'verify that the result is correct when you put in presentPrame',
-    credential,
-    presentationFrame,
-  });
-});
-
-app.post('/tests/present/p1', async (c) => {
-  const sdjwt = await getSDJwt();
-  const { answer } = await c.req.json<{ answer: string }>();
-
-  if (!answer) {
-    throw new HTTPException(400, { message: 'bad request' });
-  }
-
-  if (typeof answer !== 'string') {
-    throw new HTTPException(400, {
-      message: 'bad request You didnt put an object or JSON type in there, did you? You need to insert a token!',
-    });
-  }
-
-  const submittedClaims = await sdjwt.getClaims(answer);
-  const isCorrect = isEqual(submittedClaims, presentationClaim);
-  return c.json({ isCorrect });
-});
-
-/*
- * 문제3 :
- */
-app.get('/test/issue/p2', async (c) => {
-  return c.json({
-    description: 'verify JWT headers are compliant with the standard',
-    claims,
-    disclosureFrame,
-  });
-});
-
-app.post('/tests/issue/p2', async (c) => {
-  const sdjwt = await getSDJwt();
-  const { answer } = await c.req.json();
-
-  const header = await sdjwt.decode(answer);
-  const isCorrect = header.jwt?.header?.typ === 'sd-jwt';
-
-  return c.json({ isCorrect });
-});
-
-/*
- * 문제5 :
- */
-app.get('/test/issue/p3', async (c) => {
-  return c.json({
-    description: 'test that the claims declared to be SD are processed (set) correctly.',
-    claims,
-    disclosureFrame,
-  });
-});
-
-app.post('/tests/issue/p3', async (c) => {
-  const sdjwt = await getSDJwt();
   try {
     const { answer } = await c.req.json();
+    const isCorrect = isEqual(answer, claims);
+    return c.json({ results: isCorrect });
+  } catch (error) {
+    return c.json({ results: false });
+  }
+});
+
+app.get('/tests/present/:name', (c) => {
+  const description = 'Ill give you a token and a presentationFrame, so issue the presented token !';
+  const name = c.req.param('name') as keyof typeof presentTestCases;
+
+  if (!presentedNames.includes(name)) {
+    throw new HTTPException(404, { message: 'test not found' });
+  }
+
+  const { credential, presentationFrame } = presentTestCases[name];
+
+  return c.json({ results: { description, credential, presentationFrame } });
+});
+
+app.post(`/tests/present/:name`, async (c) => {
+  const name = c.req.param('name') as keyof typeof presentTestCases;
+  if (!presentedNames.includes(name)) {
+    throw new HTTPException(404, { message: 'test not found' });
+  }
+
+  const sdjwt = await getSDJwt();
+
+  const { claims } = presentTestCases[name];
+
+  try {
+    const { answer } = await c.req.json(); //get a token
+    await sdjwt.validate(answer);
 
     const submittedClaims = await sdjwt.getClaims(answer);
     const isCorrect = isEqual(submittedClaims, claims);
-
-    if (!isCorrect) {
-      return c.json({ isCorrect: false });
-    }
-
-    const x = await sdjwt.presentableKeys(answer);
-    const isCorrectSdClaim = isEqual(x, presentableKeyList);
-    return c.json({ isCorrect: isCorrectSdClaim });
+    return c.json({ results: isCorrect });
   } catch (error) {
-    return c.json({ isCorrect: false });
+    return c.json({ results: false });
   }
 });
+
+app.get('/tests/verify/:name', (c) => {
+  const description = 'prove that its the right token or not';
+  const name = c.req.param('name') as keyof typeof verifyTestCases;
+
+  if (!verifyedNames.includes(name)) {
+    throw new HTTPException(404, { message: 'test not found' });
+  }
+
+  const { credential } = verifyTestCases[name];
+
+  return c.json({ results: { description, credential } });
+});
+
+app.post(`/tests/verify/:name`, async (c) => {
+  const name = c.req.param('name') as keyof typeof verifyTestCases;
+  if (!verifyedNames.includes(name)) {
+    throw new HTTPException(404, { message: 'test not found' });
+  }
+
+  const sdjwt = await getSDJwt();
+  const { result } = verifyTestCases[name];
+
+  try {
+    const { answer } = await c.req.json();
+    return c.json({ results: answer === result });
+  } catch (error) {
+    return c.json({ results: false });
+  }
+});
+
+app.get('/tests/issue/:name', (c) => {
+  const description = 'I will give you claim, you have to send me a token!';
+  const name = c.req.param('name') as keyof typeof issueTestCases;
+
+  if (!verifyedNames.includes(name)) {
+    throw new HTTPException(404, { message: 'test not found' });
+  }
+
+  // TODO: check type
+  const { disclosureFrame, claims } = issueTestCases[name];
+
+  return c.json({ results: { description, claims, disclosureFrame } });
+});
+
+app.post(`/tests/issue/:name`, async (c) => {
+  const name = c.req.param('name') as keyof typeof issueTestCases;
+  if (!issuedNames.includes(name)) {
+    throw new HTTPException(404, { message: 'test not found' });
+  }
+
+  const sdjwt = await getSDJwt();
+  const { claims } = issueTestCases[name];
+
+  try {
+    const { answer } = await c.req.json(); //get a token
+    await sdjwt.validate(answer);
+
+    const submittedClaims = await sdjwt.getClaims(answer);
+    const isCorrect = isEqual(submittedClaims, claims);
+    return c.json({ results: isCorrect });
+  } catch (error) {
+    return c.json({ results: false });
+  }
+});
+
+const port = 5600;
+console.log(`Server is running on port ${port}`);
 
 serve({
   fetch: app.fetch,
